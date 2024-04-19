f2ef45acdbb55b3c8ec9e57cb60f3725410afea2
ephrem <ephremmamo555@gmail.com>
2024-04-19 23:14:51 +0300

    updated login

--- vote_sphere/lib/presentation/screens/login.dart
+++ vote_sphere/lib/presentation/screens/login.dart
@@ -1,11 +1,12 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:votesphere/Utils/extensions.dart';
-import 'package:votesphere/feedback.dart';
-import 'package:votesphere/widgets/forms.dart';
+import '../../Utils/extensions.dart';
+import 'feedback.dart';
+import '../widgets/forms.dart';
 import 'signUp.dart';
+import 'home.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -18,30 +19,29 @@ class _LoginPageState extends State<LoginPage> {
   static final formkey = new GlobalKey<FormState>();
 
   Widget build(BuildContext context) {
-
     return GestureDetector(
       onTap: () {
-
         FocusScopeNode currentFocus = FocusScope.of(context);
         if (!currentFocus.hasPrimaryFocus) {
           currentFocus.unfocus();
         }
       },
       child: Scaffold(
-
         backgroundColor: Colors.white,
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white,
           leading: IconButton(
-            onPressed: (){
+            onPressed: () {
               Navigator.pop(context);
-            }, icon: const Icon(Icons.arrow_back_ios,
-          size: 20,
-          color: Colors.black,),
-          ), systemOverlayStyle: SystemUiOverlayStyle.dark,
-
-
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            ),
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
         ),
         body: SingleChildScrollView(
           child: Container(
@@ -50,164 +50,157 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Expanded(child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        const Text("Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30
-                        ),),
-                        const SizedBox(height: 20,),
-                        Text("Login to your account",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.grey[700]
-                        ),)
-
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Column(
+                Expanded(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
-
-                          Form(
-                              key: formkey,
-                              child: Column(
-                                  children: <Widget>[
-
+                      Column(
+                        children: <Widget>[
+                          const Text(
+                            "Login",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 30),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Login to your account",
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[700]),
+                          )
+                        ],
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 40),
+                          child: Column(
+                            children: <Widget>[
+                              Form(
+                                  key: formkey,
+                                  child: Column(children: <Widget>[
                                     CustomForm(
-
                                         hintText: "Email",
-                                        validator: (val){
-                                          if (!val!.isValidEmail){
+                                        validator: (val) {
+                                          if (!val!.isValidEmail) {
                                             return "Please enter the valid email";
-                                          }else{
+                                          } else {
                                             return null;
                                           }
                                         }),
                                     CustomForm(
-
                                         hintText: "Password",
                                         obsecuretext: true,
-                                        validator: (val){
-                                          if (!val!.isValidPassword){
+                                        validator: (val) {
+                                          if (!val!.isValidPassword) {
                                             return "Please enter the valid password";
-                                          }else{
+                                          } else {
                                             return null;
                                           }
                                         }),
-
                                   ])),
-
-                        ],
-                      )),
-                      Padding(padding:
-                      EdgeInsets.symmetric(horizontal: 40),
-
-                      child: Container(
-                        padding: EdgeInsets.only(top: 1, left: 1),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          border: Border(
-                            bottom: BorderSide(color: Colors.amberAccent),
-                            top: BorderSide(color: Colors.amberAccent),
-                            right:BorderSide(color: Colors.amberAccent),
-                            left: BorderSide(color: Colors.amberAccent)
-
-                          )
-                        ),
-                        child: MaterialButton(
-                          minWidth: double.infinity,
-                          height: 60,
-                          color: Colors.lightBlue,
-                          onPressed: (){
-                            if (formkey.currentState!.validate()){}
-                          },
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)
-                          ),
-                          child: Text("Login",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18,
-                            color: Colors.white,
-                          ),),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Don't you have an account"),
-                        GestureDetector(
-                          onTap: () {
-                            // Navigate to the second page
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => SignUpPage()),
-                            );
-                          },
-                          child: Text(
-                            ' Sign up',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.blue,
-
+                            ],
+                          )),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        child: Container(
+                          padding: EdgeInsets.only(top: 1, left: 1),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border(
+                                  bottom: BorderSide(color: Colors.amberAccent),
+                                  top: BorderSide(color: Colors.amberAccent),
+                                  right: BorderSide(color: Colors.amberAccent),
+                                  left: BorderSide(color: Colors.amberAccent))),
+                          child: MaterialButton(
+                            minWidth: double.infinity,
+                            height: 60,
+                            color: Colors.lightBlue,
+                            onPressed: () {
+                              if (formkey.currentState!.validate()) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Home()));
+                              }
+                            },
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 100),
-                      height: 200,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("lib/assets/key.png"),
-                          fit: BoxFit.fitHeight
-                        )
                       ),
-                    ),
-                    Padding(
-                      
-                      padding: EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        
-                        children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("Don't you have an account"),
                           GestureDetector(
                             onTap: () {
                               // Navigate to the second page
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) => FeedBackForm()),
+                                MaterialPageRoute(
+                                    builder: (context) => SignUpPage()),
                               );
                             },
                             child: Text(
-                              'feedback',
+                              ' Sign up',
                               style: TextStyle(
                                 fontSize: 18,
                                 color: Colors.blue,
-                      
                               ),
                             ),
                           ),
-                          Icon(Icons.question_mark, color: Colors.blue, size: 18,)
                         ],
                       ),
-                    )
-                  ]
-                )
-                )
+                      Container(
+                        padding: EdgeInsets.only(top: 100),
+                        height: 200,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("../../../assets/key.png"),
+                                fit: BoxFit.fitHeight)),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                // Navigate to the second page
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => FeedBackForm()),
+                                );
+                              },
+                              child: Text(
+                                'feedback',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.blue,
+                                ),
+                              ),
+                            ),
+                            Icon(
+                              Icons.question_mark,
+                              color: Colors.blue,
+                              size: 18,
+                            )
+                          ],
+                        ),
+                      )
+                    ]))
               ],
             ),
-
-
           ),
         ),
       ),
