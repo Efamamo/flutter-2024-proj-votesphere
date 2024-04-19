4c6796ee189d5fbf28c018fa37814b1c150ba7b1
ephrem <ephremmamo555@gmail.com>
2024-04-19 23:14:09 +0300

    updated landing page

--- vote_sphere/lib/presentation/screens/landing_page.dart
+++ vote_sphere/lib/presentation/screens/landing_page.dart
@@ -5,115 +5,104 @@ import 'signUp.dart';
 import 'login.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: SafeArea(
-
-
         child: SingleChildScrollView(
-
-
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Column(
-                    children: <Widget>[
-
-                      Text(
-                          "Welcome",
-                          style: TextStyle(
+                Column(children: <Widget>[
+                  Text("Welcome",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                      width: 200,
+                      child: TextLiquidFill(
+                        text: 'Votosphere',
+                        waveColor: Colors.blueAccent,
+                        boxBackgroundColor: Colors.white,
+                        textStyle: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        boxHeight: 80,
+                      )),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  AnimatedTextKit(
+                    animatedTexts: [
+                      WavyAnimatedText(
+                          "The most competitive vote app. Vote anything that you want!",
+                          textStyle: TextStyle(
+                              color: Colors.lightBlue,
                               fontWeight: FontWeight.bold,
-                              fontSize: 30
-                          )
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                          width: 200,
-                          child: TextLiquidFill(
-                            text: 'Votosphere',
-                            waveColor: Colors.blueAccent,
-                            boxBackgroundColor: Colors.white,
-                            textStyle: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            boxHeight: 80,
-                          )),
-                      SizedBox(height: 15,),
-                      AnimatedTextKit(animatedTexts: [
-
-                        WavyAnimatedText("The most competitive vote app. Vote anything that you want!",textStyle: TextStyle(color: Colors.lightBlue,fontWeight: FontWeight.bold ,fontSize: 14), textAlign: TextAlign.center),
-                      ],
-                        isRepeatingAnimation: true,)
-
-                    ]),
+                              fontSize: 14),
+                          textAlign: TextAlign.center),
+                    ],
+                    isRepeatingAnimation: true,
+                  )
+                ]),
                 Container(
-                  height: MediaQuery.of(context).size.height/3,
+                  height: MediaQuery.of(context).size.height / 3,
                   decoration: BoxDecoration(
                       image: DecorationImage(
-                          image: AssetImage("lib/assets/images.png")
-                      )
-
-
-                  ),
+                          image: AssetImage("../../../assets/images.png"))),
                 ),
                 Column(
                   children: <Widget>[
                     MaterialButton(
                       minWidth: double.infinity,
                       height: 60,
-
-                      onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginPage() ));
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()));
                       },
                       shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                              color: Colors.amberAccent
-                          ),
-                          borderRadius: BorderRadius.circular(50)
-                      ),
-                      child: Text("Login",
+                          side: BorderSide(color: Colors.amberAccent),
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Text(
+                        "Login",
                         style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18
-                        ),),),
+                            fontWeight: FontWeight.w600, fontSize: 18),
+                      ),
+                    ),
                     SizedBox(height: 20),
                     MaterialButton(
                       minWidth: double.infinity,
-                      height:60,
-                      onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUpPage()));
+                      height: 60,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignUpPage()));
                       },
                       color: Color(0xff0095FF),
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)
-                      ),
+                          borderRadius: BorderRadius.circular(50)),
                       child: Text(
                         "Sign Up",
                         style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600
-                        ),
+                            fontSize: 18, fontWeight: FontWeight.w600),
                       ),
                     ),
-
                   ],
                 )
-
               ]),
         ),
       ),
     );
   }
 }
-
-
