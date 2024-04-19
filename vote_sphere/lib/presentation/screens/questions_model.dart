c04cfb6bfe31f73189716c3b99fa82a82ddf1ae5
ephrem <ephremmamo555@gmail.com>
2024-04-19 21:54:52 +0300

    questions class created

---
+++ vote_sphere/lib/presentation/screens/questions_model.dart
@@ -0,0 +1,7 @@
+class Question {
+  Question(
+      {required this.question, required this.comment, required this.answers});
+  String question;
+  List<String> answers;
+  List<String> comment;
+}
